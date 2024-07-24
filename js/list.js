62895b067e125cd81a9fff69e3db95dc0391527b
chocolalatte <iamduck18@gmail.com>
2024-07-24 11:07:28 -0400

    Initial commit

---
+++ js/list.js
@@ -0,0 +1,44 @@
+var nodeList = document.getElementsByName("LI");
+var a;
+for (a = 0; a < nodeList.length; a++) {
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[a].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var b;
+for (b = 0; b < close.length; b++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("listInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Enter item name");
+  } else {
+    document.getElementById("groceryList").appendChild(li);
+  }
+  document.getElementById("listInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+} 
