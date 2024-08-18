edff81ce838c949955858a0ae56489883b134f99
chocolalatte <iamduck18@gmail.com>
2024-08-18 17:33:30 -0400

    fixing time :p

--- js/list.js
+++ js/list.js
@@ -1,14 +1,16 @@
 // Declare variables for DOM objects
 const list = document.querySelector('ul')
 const input = document.getElementById('listInput');
+var itemsRQ = new XMLHttpRequest();
+let checked;
 
 // Add listItem 
-function newElement() {
+function newItem() {
   // Creates new list item
   if (input.value) {
     list.insertAdjacentHTML(
       "afterbegin",
-      `<li class="">${input.value}<span class="close">&#215;</span></li>`
+      `<li class="">${String(input.value)}<span class="close">&#215;</span></li>`
     );
   // Prompt user if item has no name
   } else {
@@ -29,3 +31,28 @@ list.addEventListener('click', event => {
       event.target.parentElement.remove();
   }
 });
+
+// itemsRQ for getting list items
+itemsRQ.onreadystatechange = function() {
+  if (itemsRQ.readyState === 4) {
+    // Check for errors
+    if (itemsRQ.status === 200) {
+      var listEnties = JSON.parse(itemsRQ.responseText);
+      // Add items to list from list.json
+      listEnties.forEach(entry => {
+        console.log(entry)
+        item = list.insertAdjacentHTML(
+          "afterbegin",
+          `<li class="">${entry.name}<span class="close">&#215;</span></li>`
+        );
+        if (entry.isChecked) {
+          item.classList.toggle('checked');
+        }
+      });
+      // Log error message
+    } else {alert(`XMLHttpRequest error: ${itemsRQ.status}`)}
+  }
+}
+// Open and send itemsRQ
+itemsRQ.open('GET', '../list.json');
+itemsRQ.send();
